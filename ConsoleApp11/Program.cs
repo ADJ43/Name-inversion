022ab9faad088378144e3fc9e41b8e459948af6a
ADJ43 <adjse43@gmail.com>
2023-12-10 15:03:56 -0500

    first commit

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name and press enter");
+            var name = Console.ReadLine();
+
+            var length = name.Length;
+
+            string[] array = new string[length];
+
+            for (int i = length -1; i >= 0; i--)
+            {
+                array[i] = name[i].ToString();
+            }
+
+
+            for (int i = length - 1; i >= 0; i--)
+            {
+                Console.Write($"{array[i]}");
+            }
+        }
+    }
+}
